6fd473626dd7fa500c3bb11fc11ba12ad33a5274
Camilo Cunha de Azevedo <camilotk@gmail.com>
2018-07-14 05:55:11 -0300

    solucionado o problema de link com o css e js local porcausa do redirect

--- application/views/produtos/index.php
+++ application/views/produtos/index.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" href="http://localhost/codeigniter_estudo/css/bootstrap.min.css">
+<script src="http://localhost/codeigniter_estudo/js/bootstrap.min.js"></script>
+</head>
 <body>
-    <?=var_dump($produtos)?>
+    <div class="container">
+    <h1>Produtos</h1>
+    <table class="table">
+        <?PHP foreach ($produtos as $produto) : ?>
+        <tr>
+            <td><?=$produto["nome"]?></td>
+            <td><?=$produto["preco"]?></td>
+        </tr>
+        <?PHP endforeach ?>
+    </table>
+    </div>
 </body>
 </html>
