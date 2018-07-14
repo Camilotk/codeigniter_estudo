735481d7ffafef44db3f2698be9751af857bd287
Camilo Cunha de Azevedo <camilotk@gmail.com>
2018-07-14 20:20:44 -0300

    Adicionado os formulários de cadastro

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -6,6 +6,8 @@ class Produtos extends CI_Controller {
         $produtos = $this->produtos_model->buscaTodos();
         $dados = array("produtos" => $produtos);
         $this->load->helper('currency');
+        $this->load->helper('url');
+        $this->load->helper('form');
         $this->load->view("produtos/index.php", $dados);
     }
 }
--- application/views/produtos/index.php
+++ application/views/produtos/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="http://localhost/codeigniter_estudo/css/bootstrap.min.css">
-<script src="http://localhost/codeigniter_estudo/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="<?=base_url("css/bootstrap.min.css")?>">
+<script src="<?=base_url("js/bootstrap.min.js")?>"></script>
 </head>
 <body>
     <div class="container">
@@ -15,6 +15,38 @@
         </tr>
         <?PHP endforeach ?>
     </table>
+
+    <h1>Cadastro</h1>
+    <?PHP
+    echo form_open("usuarios/novo");
+    echo form_label("Nome", "inNome");
+    echo form_input(array(
+        "name" => "Nome",
+        "id" => "inNome",
+        "class" => "form-control",
+        "maxlength" => "75"
+    ));
+    echo form_label("E-mail", "inEmail");
+    echo form_input(array(
+        "name" => "Email",
+        "id" => "inEmail",
+        "class" => "form-control",
+        "maxlength" => "75"
+    ));
+    echo form_label("Senha", "inSenha");
+    echo form_password(array(
+        "name" => "Senha",
+        "id" => "inSenha",
+        "class" => "form-control"
+    ));
+    echo form_button(array(
+        "name" => "Cadatrar",
+        "id" => "btnCadastrar",
+        "content" => "Cadastrar",
+        "class" => "btn btn-primary"
+    ));
+    echo form_close();
+    ?>
     </div>
 </body>
 </html>
